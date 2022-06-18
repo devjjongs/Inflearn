51ee1cc274c20ff09f51210c2610299be5701d9d
JongSeok Baek <84493338+devjjongs@users.noreply.github.com>
2022-06-19 04:12:03 +0900

    단방향 연관관계 매핑

--- jpa-mapping/src/main/java/jpateam/jpamapping/MappingMain.java
+++ jpa-mapping/src/main/java/jpateam/jpamapping/MappingMain.java
@@ -23,15 +23,29 @@ public class MappingMain {
 
         try {
 
+            // 팀 저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
+            // 회원 저장
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
 
+            // Query
+            em.flush();
+            em.clear();
+
+            // 조회
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam : " + findTeam.getName());
+
+            // 수정
+            Team newTeam = em.find(Team.class, 100L);
+            findMember.setTeam(newTeam);
             // 커밋(커밋하는 순간 insert sql을 DB에 전달)
             tx.commit();
         } catch (Exception e) {
